bb7f6663a8db3b058b8072e9fda4cc659b943395
Truong <trg552003@gmail.com>
2024-08-23 14:38:14 +0700

    init server

--- back-end/app/main.py
+++ back-end/app/main.py
@@ -1,14 +1,20 @@
 from fastapi import FastAPI, HTTPException, Depends
+from fastapi.middleware.cors import CORSMiddleware
 from db_connector import engine, Base, SessionLocal
 
 Base.metadata.create_all(bind=engine) # create all tables in database
 
 app = FastAPI()
 
+# allow NextJS, ReactJS to bypass CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['http://localhost:3000'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
 
-
-
-
-
-
-
+@app.get("/")
+def connection_check():
+    return {"message": "connect to server successfully!"}
