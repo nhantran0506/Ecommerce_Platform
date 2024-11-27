aee5b72222c0f2d19eb386c59c50bbe3896cb9ea
Nhan Tran <107875597+nhantran0506@users.noreply.github.com>
2024-11-27 22:57:43 +0700

    hot fix user apis

--- back-end/app/views/UserViews.py
+++ back-end/app/views/UserViews.py
@@ -110,17 +110,17 @@ async def signup(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
         )
 
-
-@router.get("/{user_id}")
-async def get_user(user_id: str, user_controller: UserController = Depends()):
+@router.post("/get_current_user")
+async def get_current_user(user_controller: UserController = Depends(), current_user=Depends(token_config.get_current_user)):
     try:
-        return await user_controller.get_user_by_id(user_id)
+        return await user_controller.get_current_user(current_user=current_user)
     except Exception as e:
         logger.error(str(e))
         return JSONResponse(
             content={"Message": "Unexpected error"},
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
         )
+
 
 
 @router.post("/update_user")
@@ -161,6 +161,18 @@ async def forgot_password(
 ):
     try:
         return await user_controller.forgot_password(user_data)
+    except Exception as e:
+        logger.error(str(e))
+        return JSONResponse(
+            content={"Message": "Unexpected error"},
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        )
+
+
+@router.get("/{user_id}")
+async def get_user(user_id: str, user_controller: UserController = Depends()):
+    try:
+        return await user_controller.get_user_by_id(user_id)
     except Exception as e:
         logger.error(str(e))
         return JSONResponse(
