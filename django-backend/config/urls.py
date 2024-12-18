2059fa7f12ec5720874c1da81e9156c7619b582e
Nhan Tran <107875597+nhantran0506@users.noreply.github.com>
2024-12-19 00:14:07 +0700

    modify the url route for swagger config

--- django-backend/config/urls.py
+++ django-backend/config/urls.py
@@ -33,7 +33,17 @@ schema_view = get_schema_view(
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("docs/", include([
-        path("apps/", include('apps.api.urls')),
+        path('shops/', include('apps.Shops.urls')),
+        path('users/', include('apps.Users.urls')),
+        path('products/', include('apps.Products.urls')),
+        path('categories/', include('apps.Categories.urls')),
+        path('carts/', include('apps.Carts.urls')),
+        path('orders/', include('apps.Orders.urls')),
+        path('admin/', include('apps.Admin.urls')),
+        path('chatbot/', include('apps.ChatBot.urls')),
+        path('recommendations/', include('apps.Recommendation.urls')),
+        path('embedding/', include('apps.Embedding.urls')),
+        path('notifications/', include('apps.Notify.urls')),
         path('swagger/schema/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger-schema')
     ]))
 ]
